07f0d7ed73c92dac9a014543e44a07459141a238
Rosi Volgarin <rosi.volgarin@gmail.com>
2020-12-23 22:35:42 -0300

    Devise user and admim mais devise i18n

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -11,6 +11,9 @@ Rails.application.configure do
   config.cache_classes = false
   config.action_view.cache_template_loading = true
 
+  # i18n parameters
+  config.i18n.available_locales = [:en, 'pt-br', :pt]
+
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
