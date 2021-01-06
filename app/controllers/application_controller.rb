d7f536e59e99a7dbac733bb2d9559b767b684d93
Rosi Volgarin <rosi.volgarin@gmail.com>
2021-01-06 15:10:08 -0300

    Ajustando conteudo da pagina de servicos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
     #before_action :authenticate_user!
+
+    around_action :switch_locale
+
+    def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+    end
+
 end
