979ef624798fa9a382210deb836fe4bfc157a947
Rosi Volgarin <rosi.volgarin@gmail.com>
2021-01-24 18:03:55 -0300

    Alteracao views devise

--- Gemfile
+++ Gemfile
@@ -39,6 +39,8 @@ gem 'devise-i18n'
 # Views
 gem 'devise-bootstrap-views', '~> 1.0'
 
+gem 'jquery-rails'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,12 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :users
 
-  devise_scope :user do  
-    get '/users/sign_out' => 'devise/sessions#destroy'    
-  end
-  
+  resources :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   get "/about", to: "layouts#about"
-  get "/services", to: "layouts#services" 
+  get "/services", to: "layouts#services"
 
   scope "(:locale)", :locale => /en|pt-br|pt/ do
     root "layouts#index" 
